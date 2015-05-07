30b0ff4d8f81b6a66d861d2eed1cdd70870ca528
tserenda <tserenda@hotmail.com>
2015-05-07 12:59:45 +0900

    Reorg

--- RP/assignment2/cachematrix.R
+++ RP/assignment2/cachematrix.R
@@ -1,32 +1,27 @@
 # makes "special" matrix that is list of functions on x
-
 makeCacheMatrix <- function(x = matrix()) {
-    cm <- NULL # cached matrix
-
-    # get & set matrix
-    set <- function(y) { x <<- y; cm <- NULL }
-    get <- function() x
-
-    # get & set cached matrix
-    setInv <- function(inv) { cm <<- inv }
-    getInv <- function() cm
-    
-    list(set = set, get = get, setInv = setInv, getInv = getInv)
+        cm <- NULL # cached matrix
+        
+        # get & set matrix
+        set <- function(y) { x <<- y; cm <- NULL }
+        get <- function() { x }
+        
+        # get & set cached matrix
+        setInv <- function(inv) { cm <<- inv }
+        getInv <- function() { cm }
+        
+        list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
-# return the cached inverse of matrix or 
-# calculate the inverse if it hasn't been
-
+# return the cached inverse of matrix or calculate the inverse if it hasn't been
 cacheSolve <- function(x, ...) {
-
-    inv <- x$getInv()
-    if (!is.null(inv)) {
-        message("getting cached inverse")
-        return(inv)
-    }
-    
-    data <- x$get()
-    inv <- solve(data)
-    x$setInv(inv)
-    inv
+        inv <- x$getInv()
+        if (! is.null(inv)) {
+                message("getting cached inverse")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data) # solve matrix inverse
+        x$setInv(inv)
+        inv
 }
